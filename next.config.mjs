9a032fd7348f4141b9156a4bad32504a8a47309e
Gercino Luiz <gercino.neto.luiz@gmail.com>
2026-01-22 11:53:23 -0300

    Fix next Configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-  // output: "export", // Uncomment if static export is needed (Vercel usually handles default)
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
-    ignoreBuildErrors: true, 
+    ignoreBuildErrors: true,
   }
 };
 
